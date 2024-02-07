ff9fcc64bfc869ae616a2dd3b008449d5278b30e
Henrique Baldo Carlos <80064286+boldoboldin@users.noreply.github.com>
2024-02-07 09:28:42 -0300

    States adjustments

--- Assets/Scripts/WorkerBeeCtrl.cs
+++ Assets/Scripts/WorkerBeeCtrl.cs
@@ -13,7 +13,7 @@ public class WorkerBeeCtrl : Bee
 
     [SerializeField] private GameObject nest;
 
-    ForagingStates currentState;
+    private string currentAction;
 
     public static List<Bee> moveableUnits = new List<Bee>();
     public NavMeshAgent agent;
@@ -23,13 +23,15 @@ public class WorkerBeeCtrl : Bee
     [SerializeField] private CircleCollider2D visionCol;
     [SerializeField] private Transform[] flowersPos;
 
+    private float delay = 0f;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
 
-        currentState = new Idle(gameObject, agent, visionCol, flowersPos);
+        //currentState = new Idle(gameObject, agent, visionCol, flowersPos);
 
         moveableUnits.Add(this);
         destination = transform.position;
@@ -37,22 +39,40 @@ public class WorkerBeeCtrl : Bee
 
     void Update()
     {
-        currentState = currentState.Process();
+        //currentState = currentState.Process();
         
         if (Input.GetMouseButtonDown(0) && !isSelected )
         {
-            //Debug.Log("Unit" + this.name + " selected");
+            //info
         }
-
-        if (Input.GetMouseButtonDown(0) && isSelected)
+        else if (Input.GetMouseButtonDown(0) && isSelected)
         {
+            Debug.Log("Unit" + this.name + " selected");
             SetAction();
+
         }
 
         if (Input.GetMouseButtonDown(1))
         {
             Debug.Log("Deselected units");
             DeselectUnits();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B) && isSelected)
+        {
+            currentAction = "Browsing";
+            Debug.Log("The bee " + this.name + " is browsing for flowers");
+            
+        }
+
+        if (currentAction == "Browsing")
+        {
+            Browsing();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && isSelected)
+        {
+            //currentState = new Idle(gameObject, agent, visionCol, flowersPos);
         }
     }
 
@@ -71,8 +91,6 @@ public class WorkerBeeCtrl : Bee
             if (hit.collider.CompareTag("Flower"))
             {
                 Move(hit.point);
-
-
             }
             else if (hit.collider.CompareTag("Nest"))
             {
@@ -86,12 +104,6 @@ public class WorkerBeeCtrl : Bee
             Debug.Log("Clicked on: " + hit.collider.name + " " + hit.point);
 
         }
-    }
-
-    private void Move(Vector3 destination)
-    {
-        agent.stoppingDistance = 0;
-        agent.SetDestination(destination);
     }
 
     private void DeselectUnits()
@@ -120,9 +132,64 @@ public class WorkerBeeCtrl : Bee
         }
     }
 
-    private void Browsing(string resource, Vector2 areaTarget)
-    {
 
+    private void Move(Vector3 destination)
+    {
+        agent.stoppingDistance = 0;
+        agent.SetDestination(destination);
+        currentAction = "Idle";
+        agent.speed = 2f;
+
+    }
+
+    private void Browsing()
+    {
+        delay = delay - 0.1f;
+
+        agent.isStopped = false;
+        agent.speed = 1f;
+        //Animator
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f && delay <= 0)
+        {
+            SetRandomDestination();
+            delay = Random.Range(100f, 500f);
+
+        }
+
+        //if (CanSeeFlowers())
+        //{
+        //    Debug.Log("The bee " + workerBee.name + " found a flower");
+        //}
+
+        //public bool CanSeeFlowers()
+        //{
+        //    Collider2D[] collisions = Physics2D.OverlapCircleAll(visionCol.transform.position, visionCol.radius);
+        //    foreach (Collider2D col in collisions)
+        //    {
+        //        if (col.CompareTag("Flower"))
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
+    }
+
+    private void SetRandomDestination()
+    {
+        float rndDestRange = 5f;
+
+        Vector2 rndDirection = Random.insideUnitCircle.normalized * rndDestRange;
+        Vector2 rndDest = (Vector2)transform.position + rndDirection;
+        NavMeshHit hit;
+
+
+        if (NavMesh.SamplePosition(rndDest, out hit, rndDestRange, NavMesh.AllAreas))
+        {
+            agent.SetDestination(hit.position);
+            return;
+        }
     }
 
     private void Collect(string resource, GameObject plantTarget)
